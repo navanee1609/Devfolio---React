b74b7fa1125090a2b1d930c14d8afe27692488e4
navanee1609 <navaneethanvs18@gmail.com>
2023-11-01 00:03:20 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
-// import { darkTheme, lightTheme } from './utils/Themes.js'
+import { darkTheme, lightTheme } from './utils/Themes.js'
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -32,7 +32,7 @@ function App() {
   console.log(openModal)
   return (
     <ThemeProvider 
-    // theme={darkMode ? darkTheme : lightTheme}
+    theme={darkMode ? darkTheme : lightTheme}
     >
       <Router >
         <Navbar />
