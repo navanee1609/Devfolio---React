7f413b3b0acec77929327468a26296b7d323b0db
Navaneethan KV <navaneethanvs18@gmail.com>
2024-01-03 20:49:53 +0530

    removed unused

--- src/data/constants.js
+++ src/data/constants.js
@@ -82,9 +82,7 @@ export const skills = [
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
       },
-      {
-
-      }
+      
       
     ],
   },
