48b050a0c272c9d8221b7576ffe647a7e1a43d9a
navanee1609 <navaneethanvs18@gmail.com>
2024-03-15 16:40:26 +0530

    added innovative functionality and added vadditional features

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,172 +1,172 @@
-// import React, { useRef, useState } from 'react';
-// import styled from 'styled-components';
-// import { Snackbar, Alert } from '@mui/material';
+// // // // import React, { useRef, useState } from 'react';
+// // // // import styled from 'styled-components';
+// // // // import { Snackbar, Alert } from '@mui/material';
 
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   position: relative;
-//   z-index: 1;
-//   align-items: center;
-//   @media (max-width: 960px) {
-//     padding: 0px;
-//   }
-// `;
+// // // // const Container = styled.div`
+// // // //   display: flex;
+// // // //   flex-direction: column;
+// // // //   justify-content: center;
+// // // //   position: relative;
+// // // //   z-index: 1;
+// // // //   align-items: center;
+// // // //   @media (max-width: 960px) {
+// // // //     padding: 0px;
+// // // //   }
+// // // // `;
 
-// const Wrapper = styled.div`
-//   position: relative;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   flex-direction: column;
-//   width: 100%;
-//   max-width: 1350px;
-//   padding: 0px 0px 80px 0px;
-//   gap: 12px;
-//   @media (max-width: 960px) {
-//     flex-direction: column;
-//   }
-// `;
+// // // // const Wrapper = styled.div`
+// // // //   position: relative;
+// // // //   display: flex;
+// // // //   justify-content: space-between;
+// // // //   align-items: center;
+// // // //   flex-direction: column;
+// // // //   width: 100%;
+// // // //   max-width: 1350px;
+// // // //   padding: 0px 0px 80px 0px;
+// // // //   gap: 12px;
+// // // //   @media (max-width: 960px) {
+// // // //     flex-direction: column;
+// // // //   }
+// // // // `;
 
-// const Title = styled.div`
-//   font-size: 42px;
-//   text-align: center;
-//   font-weight: 600;
-//   margin-top: 20px;
-//   color: ${({ theme }) => theme.text_primary};
-//   @media (max-width: 768px) {
-//     margin-top: 12px;
-//     font-size: 32px;
-//   }
-// `;
+// // // // const Title = styled.div`
+// // // //   font-size: 42px;
+// // // //   text-align: center;
+// // // //   font-weight: 600;
+// // // //   margin-top: 20px;
+// // // //   color: ${({ theme }) => theme.text_primary};
+// // // //   @media (max-width: 768px) {
+// // // //     margin-top: 12px;
+// // // //     font-size: 32px;
+// // // //   }
+// // // // `;
 
-// const Desc = styled.div`
-//   font-size: 18px;
-//   text-align: center;
-//   max-width: 600px;
-//   color: ${({ theme }) => theme.text_secondary};
-//   @media (max-width: 768px) {
-//     margin-top: 12px;
-//     font-size: 16px;
-//   }
-// `;
+// // // // const Desc = styled.div`
+// // // //   font-size: 18px;
+// // // //   text-align: center;
+// // // //   max-width: 600px;
+// // // //   color: ${({ theme }) => theme.text_secondary};
+// // // //   @media (max-width: 768px) {
+// // // //     margin-top: 12px;
+// // // //     font-size: 16px;
+// // // //   }
+// // // // `;
 
-// const ContactForm = styled.form`
-//   width: 95%;
-//   max-width: 600px;
-//   display: flex;
-//   flex-direction: column;
-//   background-color: ${({ theme }) => theme.card};
-//   padding: 32px;
-//   border-radius: 16px;
-//   box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
-//   margin-top: 28px;
-//   gap: 12px;
-// `;
+// // // // const ContactForm = styled.form`
+// // // //   width: 95%;
+// // // //   max-width: 600px;
+// // // //   display: flex;
+// // // //   flex-direction: column;
+// // // //   background-color: ${({ theme }) => theme.card};
+// // // //   padding: 32px;
+// // // //   border-radius: 16px;
+// // // //   box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+// // // //   margin-top: 28px;
+// // // //   gap: 12px;
+// // // // `;
 
-// const ContactTitle = styled.div`
-//   font-size: 24px;
-//   margin-bottom: 6px;
-//   font-weight: 600;
-//   color: ${({ theme }) => theme.text_primary};
-// `;
+// // // // const ContactTitle = styled.div`
+// // // //   font-size: 24px;
+// // // //   margin-bottom: 6px;
+// // // //   font-weight: 600;
+// // // //   color: ${({ theme }) => theme.text_primary};
+// // // // `;
 
-// const ContactInput = styled.input`
-//   flex: 1;
-//   background-color: transparent;
-//   border: 1px solid ${({ theme }) => theme.text_secondary};
-//   outline: none;
-//   font-size: 18px;
-//   color: ${({ theme }) => theme.text_primary};
-//   border-radius: 12px;
-//   padding: 12px 16px;
-//   &:focus {
-//     border: 1px solid ${({ theme }) => theme.primary};
-//   }
-// `;
+// // // // const ContactInput = styled.input`
+// // // //   flex: 1;
+// // // //   background-color: transparent;
+// // // //   border: 1px solid ${({ theme }) => theme.text_secondary};
+// // // //   outline: none;
+// // // //   font-size: 18px;
+// // // //   color: ${({ theme }) => theme.text_primary};
+// // // //   border-radius: 12px;
+// // // //   padding: 12px 16px;
+// // // //   &:focus {
+// // // //     border: 1px solid ${({ theme }) => theme.primary};
+// // // //   }
+// // // // `;
 
-// const ContactInputMessage = styled.textarea`
-//   flex: 1;
-//   background-color: transparent;
-//   border: 1px solid ${({ theme }) => theme.text_secondary};
-//   outline: none;
-//   font-size: 18px;
-//   color: ${({ theme }) => theme.text_primary};
-//   border-radius: 12px;
-//   padding: 12px 16px;
-//   &:focus {
-//     border: 1px solid ${({ theme }) => theme.primary};
-//   }
-// `;
+// // // // const ContactInputMessage = styled.textarea`
+// // // //   flex: 1;
+// // // //   background-color: transparent;
+// // // //   border: 1px solid ${({ theme }) => theme.text_secondary};
+// // // //   outline: none;
+// // // //   font-size: 18px;
+// // // //   color: ${({ theme }) => theme.text_primary};
+// // // //   border-radius: 12px;
+// // // //   padding: 12px 16px;
+// // // //   &:focus {
+// // // //     border: 1px solid ${({ theme }) => theme.primary};
+// // // //   }
+// // // // `;
 
-// const ContactButton = styled.input`
-//   width: 100%;
-//   text-decoration: none;
-//   text-align: center;
-//   background: hsla(271, 100%, 50%, 1);
-//   background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
-//   background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
-//   background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
-//   padding: 13px 16px;
-//   margin-top: 2px;
-//   border-radius: 12px;
-//   border: none;
-//   color: ${({ theme }) => theme.text_primary};
-//   font-size: 18px;
-//   font-weight: 600;
-//   cursor: pointer;
-// `;
+// // // // const ContactButton = styled.input`
+// // // //   width: 100%;
+// // // //   text-decoration: none;
+// // // //   text-align: center;
+// // // //   background: hsla(271, 100%, 50%, 1);
+// // // //   background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+// // // //   background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+// // // //   background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+// // // //   padding: 13px 16px;
+// // // //   margin-top: 2px;
+// // // //   border-radius: 12px;
+// // // //   border: none;
+// // // //   color: ${({ theme }) => theme.text_primary};
+// // // //   font-size: 18px;
+// // // //   font-weight: 600;
+// // // //   cursor: pointer;
+// // // // `;
 
-// const AlertWrapper = styled.div`
-//   .MuiAlert-filledInfo {
-//     background-color: white !important;
-//     color: green !important;
-//     margin-top: -50px;
-//     z-index: 1000;
-//   }
+// // // // const AlertWrapper = styled.div`
+// // // //   .MuiAlert-filledInfo {
+// // // //     background-color: white !important;
+// // // //     color: green !important;
+// // // //     margin-top: -50px;
+// // // //     z-index: 1000;
+// // // //   }
 
-//   .MuiSvgIcon-root {
-//     fill: black !important;
-//   }
-// `;
+// // // //   .MuiSvgIcon-root {
+// // // //     fill: black !important;
+// // // //   }
+// // // // `;
 
-// const Contact = () => {
-//   const [open, setOpen] = useState(false);
-//   const [alertOpen, setAlertOpen] = useState(false);
-//   const form = useRef();
+// // // // const Contact = () => {
+// // // //   const [open, setOpen] = useState(false);
+// // // //   const [alertOpen, setAlertOpen] = useState(false);
+// // // //   const form = useRef();
 
-//   const handleAlertClose = () => {
-//     setAlertOpen(false);
-//   };
+// // // //   const handleAlertClose = () => {
+// // // //     setAlertOpen(false);
+// // // //   };
 
-//   return (
-//     <Container>
-//       <Wrapper>
-//         <Title>Contact</Title>
-//         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
-//         <ContactForm ref={form} action="https://formspree.io/f/xoqoobwe" method="POST">
-//           <ContactTitle>Email Me 🚀</ContactTitle>
-//           <ContactInput placeholder="Your Email" name="_replyto" />
-//           <ContactInput placeholder="Your Name" name="name" />
-//           <ContactInput placeholder="Subject" name="subject" />
-//           <ContactInputMessage placeholder="Message" rows="4" name="message" />
-//           <ContactButton type="submit" value="Send" />
-//         </ContactForm>
-//         <Snackbar open={open} autoHideDuration={9000} onClose={() => setOpen(false)} />
-//         <Snackbar open={alertOpen} autoHideDuration={9000} onClose={handleAlertClose}>
-//           <AlertWrapper>
-//             <Alert onClose={handleAlertClose} variant="filled" severity="info">
-//               Your request is being sent to Navaneethan.
-//             </Alert>
-//           </AlertWrapper>
-//         </Snackbar>
-//       </Wrapper>
-//     </Container>
-//   );
-// };
+// // // //   return (
+// // // //     <Container>
+// // // //       <Wrapper>
+// // // //         <Title>Contact</Title>
+// // // //         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+// // // //         <ContactForm ref={form} action="https://formspree.io/f/xoqoobwe" method="POST">
+// // // //           <ContactTitle>Email Me 🚀</ContactTitle>
+// // // //           <ContactInput placeholder="Your Email" name="_replyto" />
+// // // //           <ContactInput placeholder="Your Name" name="name" />
+// // // //           <ContactInput placeholder="Subject" name="subject" />
+// // // //           <ContactInputMessage placeholder="Message" rows="4" name="message" />
+// // // //           <ContactButton type="submit" value="Send" />
+// // // //         </ContactForm>
+// // // //         <Snackbar open={open} autoHideDuration={9000} onClose={() => setOpen(false)} />
+// // // //         <Snackbar open={alertOpen} autoHideDuration={9000} onClose={handleAlertClose}>
+// // // //           <AlertWrapper>
+// // // //             <Alert onClose={handleAlertClose} variant="filled" severity="info">
+// // // //               Your request is being sent to Navaneethan.
+// // // //             </Alert>
+// // // //           </AlertWrapper>
+// // // //         </Snackbar>
+// // // //       </Wrapper>
+// // // //     </Container>
+// // // //   );
+// // // // };
 
-// export default Contact;
+// // // // export default Contact;
 
 
 import React, { useRef, useState } from 'react';
@@ -294,7 +294,8 @@ const AlertWrapper = styled.div`
     background-color: white !important;
     color: green !important;
     margin-top: -50px;
-    z-index: 1000;
+    z-index: 1500; /* Increased z-index */
+    border-radius: 12px; /* Added border-radius */
   }
 
   .MuiSvgIcon-root {
@@ -302,13 +303,27 @@ const AlertWrapper = styled.div`
   }
 `;
 
+const CustomSnackbar = styled(Snackbar)`
+  z-index: 1600; /* Increased z-index */
+  border-radius: 12px; /* Added border-radius */
+`;
+
+
 export default function Contact() {
   const [result, setResult] = useState("");
+  const [buttonText, setButtonText] = useState("Send");
   const form = useRef();
+
+  const speakSuccessMessage = () => {
+    const message = "Your email sent successfully. Have a good day.";
+    const utterance = new SpeechSynthesisUtterance(message);
+    utterance.lang = "en-IN"; // Set Indian accent
+    speechSynthesis.speak(utterance);
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    setResult("Sending....");
+    setButtonText("Sending...");
     const formData = new FormData(event.target);
 
     formData.append("access_key", "89c046ff-8f76-47dd-9562-7105809a3576");
@@ -321,16 +336,20 @@ export default function Contact() {
     const data = await response.json();
 
     if (data.success) {
-      setResult("Form Submitted Successfully");
+      setResult("Form submitted successfully"); // Updated success message
+      speakSuccessMessage(); // Call text-to-speech function
+      setButtonText("Thanks for contacting");
       event.target.reset();
     } else {
       console.log("Error", data);
       setResult(data.message);
+      setButtonText("Send");
     }
   };
 
   const handleAlertClose = () => {
     setResult("");
+    setButtonText("Send"); // Reset button text when Snackbar closes
   };
 
   return (
@@ -344,16 +363,18 @@ export default function Contact() {
           <ContactInput placeholder="Your Name" name="name" required />
           <ContactInput placeholder="Subject" name="subject" />
           <ContactInputMessage placeholder="Message" rows="4" name="message" required />
-          <ContactButton type="submit">Send</ContactButton>
+          <ContactButton type="submit" disabled={buttonText === "Sending..."}>{buttonText}</ContactButton>
         </ContactForm>
-        <Snackbar open={result !== ""} autoHideDuration={9000} onClose={handleAlertClose}>
+        <CustomSnackbar open={result !== ""} autoHideDuration={5000} onClose={handleAlertClose}>
           <AlertWrapper>
             <Alert onClose={handleAlertClose} variant="filled" severity="info">
               {result}
             </Alert>
           </AlertWrapper>
-        </Snackbar>
+        </CustomSnackbar>
       </Wrapper>
     </Container>
   );
 }
+
+
