8681e525837befc838535a1548027e8efecb7670
navanee1609 <navaneethanvs18@gmail.com>
2024-04-15 15:25:32 +0530

    created an info abnd mobile icon in modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import LinkedInIcon from '@mui/icons-material/LinkedIn'; 
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import ExploreIcon from '@mui/icons-material/Explore';
+import InfoIcon from '@mui/icons-material/Info';
+import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
 
 const ModalOverlay = styled.div`
   position: fixed;
@@ -18,27 +20,28 @@ const ModalWrapper = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 95%; 
-  max-width: 600px; 
-  background-color: #222A35;
+  width: 95%;
+  max-width: 600px;
+  background-color: #222a35;
   padding: 20px;
   border-radius: 10px;
   z-index: 1000;
-  transition: transform 0.3s ease, background-color 0.3s ease; /* Added background-color transition */
+  transition: transform 0.3s ease, background-color 0.3s ease;
 
   &:hover {
     transform: translate(-50%, -50%) scale(1.02);
-    background-color: #19212C; /* Change to the desired hovering background color */
+    background-color: #19212c;
   }
 `;
 
 const ModalContent = styled.div`
   text-align: center;
+  position: relative;
 `;
 
 const CloseButton = styled.button`
-  background-color: #854CE6; 
-  color: #F2F5F7; 
+  background-color: #854ce6;
+  color: #f2f5f7;
   border: none;
   border-radius: 5px;
   padding: 10px 20px;
@@ -46,40 +49,109 @@ const CloseButton = styled.button`
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #575C66; 
+    background-color: #575c66;
   }
 `;
 
 const LinkedInLink = styled.a`
   display: inline-block;
-  margin-top: 4px;
-  color: #575C66; 
-  text-decoration: none; 
+  margin-right: 10px; // Adjust margin to separate LinkedIn and phone icon
+  color: #575c66;
+  text-decoration: none;
   transition: color 0.3s ease;
-  position: relative; 
+  position: relative;
 
   &:hover {
-    color: #854CE6; 
+    color: #854ce6;
   }
 `;
 
-const Tooltip = styled.span`
+const LinkedInTooltip = styled.span`
   visibility: hidden;
   width: 120px;
-  background-color: #575C66; 
-  color: #FFFFFF; 
+  background-color: #575c66;
+  color: #ffffff;
   text-align: center;
   border-radius: 6px;
   padding: 5px 0;
   position: absolute;
   z-index: 1;
-  bottom: calc(100% + 5px); 
+  bottom: calc(100% + 5px);
   left: 50%;
-  transform: translateX(-50%);
+  transform: translatex(-50%);
   opacity: 0;
   transition: opacity 0.3s;
 
   ${LinkedInLink}:hover & {
+    visibility: visible;
+    opacity: 1;
+  }
+`;
+
+const PhoneLink = styled.a`
+  display: inline-block;
+  color: #575c66;
+  text-decoration: none;
+  transition: color 0.3s ease;
+  position: relative;
+
+  &:hover {
+    color: #854ce6;
+  }
+`;
+
+const PhoneTooltip = styled.span`
+  visibility: hidden;
+  width: 120px;
+  background-color: #575c66;
+  color: #ffffff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: calc(100% + 5px);
+  left: 50%;
+  transform: translatex(-50%);
+  opacity: 0;
+  transition: opacity 0.3s;
+
+  ${PhoneLink}:hover & {
+    visibility: visible;
+    opacity: 1;
+  }
+`;
+
+const InfoIconWrapper = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #575c66;
+  cursor: pointer;
+  position: relative;
+
+  &:hover {
+    color: #854ce6;
+  }
+`;
+
+const InfoTooltip = styled.span`
+  visibility: hidden;
+  width: 90%;
+  background-color: #575c66;
+  color: #ffffff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: calc(100% + 5px);
+  left: 50%; // Center tooltip
+  transform: translateX(-50%);
+  opacity: 0;
+  transition: opacity 0.3s;
+
+  ${InfoIconWrapper}:hover & {
     visibility: visible;
     opacity: 1;
   }
@@ -91,7 +163,7 @@ const IconWrapper = styled.span`
 `;
 
 const ExploreText = styled.span`
-  font-size: 18px; 
+  font-size: 18px;
 `;
 
 const Modal = ({ isOpen, closeModal }) => {
@@ -102,20 +174,31 @@ const Modal = ({ isOpen, closeModal }) => {
       <ModalOverlay />
       <ModalWrapper>
         <ModalContent>
-          <h2 style={{ color: '#854CE6', marginBottom: '20px' }}>Welcome to My World of Creation!</h2>
-          <p style={{ color: '#575C66', marginBottom: '10px' }}>Hello there! I'm excited to share my work with you.</p>
-          <p style={{ color: '#575C66', marginBottom: '10px' }}>Feel free to browse through my projects and learn more about my skills and experiences.</p>
-          <p style={{ color: '#575C66', marginBottom: '10px' }}>If you have any questions or would like to collaborate, don't hesitate to reach out!</p>
-          <p style={{ color: '#575C66', marginBottom: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          {/* Info Icon with Tooltip */}
+          <InfoIconWrapper>
+            <InfoIcon />
+            <InfoTooltip>
+              Portfolio - Navaneethan KV, Front End Developer | React.js
+            </InfoTooltip>
+          </InfoIconWrapper>
+
+          <h2 style={{ color: '#854ce6', marginBottom: '20px' }}>Welcome to My World of Creation!</h2>
+          <p style={{ color: '#575c66', marginBottom: '10px' }}>Hello there! I'm excited to share my work with you.</p>
+          <p style={{ color: '#575c66', marginBottom: '10px' }}>Feel free to browse through my projects and learn more about my skills and experiences.</p>
+          <p style={{ color: '#575c66', marginBottom: '10px' }}>If you have any questions or would like to collaborate, don't hesitate to reach out!</p>
+          <p style={{ color: '#575c66', marginBottom: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             Looking forward to connect! 
             <LinkedInLink href="https://www.linkedin.com/in/navaneethan-k-v-546a9025b/" target="_blank">
               <IconWrapper><LinkedInIcon /></IconWrapper>
-              <Tooltip>Navaneethan</Tooltip>
+              <LinkedInTooltip>Navaneethan</LinkedInTooltip>
             </LinkedInLink>
+            <PhoneLink href="tel:+6380939303">
+              <IconWrapper><MobileFriendlyIcon /></IconWrapper>
+              <PhoneTooltip>Contact Me</PhoneTooltip>
+            </PhoneLink>
           </p>
           <CloseButton onClick={closeModal}>
             <ExploreText>Explore</ExploreText>
-            {/* <IconWrapper><ExploreIcon /></IconWrapper> */}
           </CloseButton>
         </ModalContent>
       </ModalWrapper>
